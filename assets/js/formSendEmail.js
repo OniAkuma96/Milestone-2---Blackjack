7a7bd3001ae63fab4fc066e8bcad4f3ca654d658
OniAkuma96 <harrison.tudball@hotmail.com>
2021-04-08 12:43:00 +0000

    email/name/message fields will return to default after successful submission

--- assets/js/formSendEmail.js
+++ assets/js/formSendEmail.js
@@ -10,6 +10,9 @@ function sendEmail(contactForm) {
     .then(
         function(response) {
             alert("Your message has been sent");
+            $("#user-input-email").html("");
+            $("#user-input-name").html("");
+            $("#user-text-area").html("");
             console.log("SUCCESS", response);
         },
         function(error) {
